2fb5cbd5266876ac2d728843d930aaecfda7b05e
Abebawushibru <abeaba64@gmail.com>
2024-02-08 14:44:08 +0300

    after linkdin link  commit

--- src/component/foooter.js
+++ src/component/foooter.js
@@ -8,9 +8,14 @@ function Footer() {
       <div className="mainContaire"  >
         <div className="container footer">
          <div className="sociallinks"><a href=" https://www.facebook.com/abebayehu.shibru.9/"> <i className="fa fa-facebook-f"></i>
-            </a> <a href="https://t.me/abebawush"> <i className="fab fa-telegram-plane"></i>
-            </a> <a href="https://www.instagram.com/abebayehushibru/"> <i className="fa fa-instagram"></i>
-            </a> </div>
+            </a> 
+            <a href="https://t.me/abebawush"> <i className="fab fa-telegram-plane"></i>
+            </a> 
+            <a href="https://www.instagram.com/abebayehushibru/"> <i className="fa fa-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/ahavahSWd/"> <i className="fa fa-linkedin"></i>
+            </a>
+             </div>
             <p>© 2023 : All Rights Reserved. Developed by <a href="home">Ahavha SWD</a></p>
      
         </div>
